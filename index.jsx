fe494c15dc51a06e369f19dfe3c8211136b5f555
Muri-tuu <kenhopkins001@gmail.com>
2024-05-31 21:36:22 +0300

    Initial Commit

---
+++ index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react';
+import App from './App';
+
+// Create a root.
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Initial render: Render an element to the root.
+root.render(<App />);
